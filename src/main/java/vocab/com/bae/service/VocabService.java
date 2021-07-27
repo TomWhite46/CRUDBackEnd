a162247804c442b5305733f85e81c08e174b278e
TomWhite46 <tomrwhite@hotmail.co.uk>
2021-07-27 10:52:53 +0100

    services added

--- src/main/java/vocab/com/bae/service/VocabService.java
+++ src/main/java/vocab/com/bae/service/VocabService.java
@@ -1,5 +1,21 @@
 package vocab.com.bae.service;
 
-public class VocabService {
+import java.util.List;
+
+import vocab.com.bae.data.Word;
+
+public interface VocabService {
+
+//	GetAll
+	public List<Word> getAll();
+
+//	Create new
+	public Word createWord(Word word);
+
+//	Delete selected
+	public String deleteWord(int id);
+
+//	Update
+	public Word replaceWord(int id, Word word);
 
 }
--- src/main/java/vocab/com/bae/service/VocabServiceDB.java
+++ src/main/java/vocab/com/bae/service/VocabServiceDB.java
@@ -1,5 +1,53 @@
 package vocab.com.bae.service;
 
-public class VocabServiceDB {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import vocab.com.bae.data.Word;
+import vocab.com.bae.data.repos.VocabRepo;
+
+@Service
+
+public class VocabServiceDB implements VocabService {
+
+	private VocabRepo repo;
+
+	// constructor
+	public VocabServiceDB(VocabRepo repo) {
+		super();
+		this.repo = repo;
+	}
+
+//	get all
+	@Override
+	public List<Word> getAll() {
+		return this.repo.findAll();
+	}
+
+//	create new
+	@Override
+	public Word createWord(Word word) {
+		this.repo.save(word);
+		return word;
+	}
+
+//	delete by id
+	@Override
+	public String deleteWord(int id) {
+		this.repo.deleteById(id);
+		return "ID " + id + " deleted.";
+	}
+
+//	replace by id with new word
+	@Override
+	public Word replaceWord(int id, Word word) {
+		Word foundWord = this.repo.findById(id).get();
+		foundWord.setIcelandic(word.getIcelandic());
+		foundWord.setEnglish(word.getEnglish());
+		foundWord.setPos(word.getPos());
+		this.repo.save(foundWord);
+		return word;
+	}
 
 }
