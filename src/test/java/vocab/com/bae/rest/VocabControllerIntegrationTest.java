bc2cd77ea585c3a39cfc4567070f2f23a5cc1923
TomWhite46 <tomrwhite@hotmail.co.uk>
2021-07-27 12:32:34 +0100

    framework for integration test

---
+++ src/test/java/vocab/com/bae/rest/VocabControllerIntegrationTest.java
@@ -0,0 +1,44 @@
+package vocab.com.bae.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+
+@Sql(scripts = { "classpath:vocab-schema.sql",
+		"classpath:vocab-schema.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+
+@AutoConfigureMockMvc
+
+@ActiveProfiles("test")
+
+public class VocabControllerIntegrationTest {
+
+	@Autowired
+	private MockMvc mockMVC;
+
+	@Autowired
+	private ObjectMapper mapper;
+
+//	test for create new word
+	@Test
+	void testCreate() throws Exception {
+
+	}
+
+//	test for getall
+	@Test
+	void testGetall() throws Exception {
+
+	}
+
+}
